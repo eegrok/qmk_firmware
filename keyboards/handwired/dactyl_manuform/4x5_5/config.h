fc0d9ea4ca9e275d4bdf6749a038c1008ef662ba
eegrok <304507+eegrok@users.noreply.github.com>
2023-04-24 20:52:44 +1200

    working with 4x5

--- keyboards/handwired/dactyl_manuform/4x5_5/config.h
+++ keyboards/handwired/dactyl_manuform/4x5_5/config.h
@@ -5,10 +5,11 @@
 
 
 // Wiring pins
-#define MATRIX_ROW_PINS { F7, B1, B3, B2, B4 }
-#define MATRIX_COL_PINS { D4, C6, D7, E6, F6 }
+#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { C6, D7, E6, B4, B5 }
 
 #define DIODE_DIRECTION COL2ROW
 
-#define EE_HANDS
-#define SPLIT_USB_DETECT
+// WS2812 RGB LED strip input and number of LEDs
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 12

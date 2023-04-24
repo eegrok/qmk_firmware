9ad58e6455155b3b426225924e65d744f20751b8
eegrok <304507+eegrok@users.noreply.github.com>
2023-04-25 08:53:19 +1200

    tweak keybindings a bit

--- keyboards/handwired/dactyl_manuform/4x5_5/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5_5/keymaps/default/keymap.c
@@ -43,12 +43,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                 KC_LBRC, KC_RBRC,                                              KC_QUOT, KC_GRV,
-                                    SH_ESC,                            KC_BSPC,
-                                    KC_LGUI, KC_TAB,          KC_RSFT, KC_SPC,
-                                    RAISE,   KC_LCTL,         LOWER,  KC_ENT
+                 KC_TAB, KC_RBRC,                                              KC_QUOT, KC_GRV,
+                                    KC_ESC,                            KC_BSPC,
+                                    KC_ENT, KC_LCTL,          KC_RSFT, KC_SPC,
+                                    KC_SPC, KC_QUOT,          LOWER,  KC_ENT
     ),
-   
+
     /* Colemak
      * ,----------------------------------,                  ,----------------------------------,
      * |   q  |   w  |   f  |   p  |   g  |                  |   j  |   l  |   u  |   y  |   ;  |
@@ -135,7 +135,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // If console is enabled, it will print the matrix position and status of each key pressed
 #ifdef CONSOLE_ENABLE
     uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %d, time: %u, interrupt: %d, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
-#endif 
+#endif
     switch (keycode) {
         case QWERTY:
             if (record->event.pressed) {
